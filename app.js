a7b585e4535dc693b1e39f75a93824e059cef06e
rabinniroula <30528863+rabinniroula@users.noreply.github.com>
2022-09-15 19:08:19 +0545

    functional with limited capabilities

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ navigator.getUserMedia = navigator.getUserMedia
     || navigator.mozGetUserMedia;
 navigator.getUserMedia({ video: false, audio: true }, callback, console.log);
 
+const mon = document.getElementById('tuneMon');
+const note = document.getElementById('note');
+
 function callback(stream) {
     ctx = new AudioContext();
     console.log(ctx);
@@ -15,29 +18,19 @@ function callback(stream) {
     draw();
 }
 
-const canvas = document.getElementById('tuneMon');
-const canvasCtx = canvas.getContext('2d');
-canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-
 function draw() {
     //Schedule next redraw
     requestAnimationFrame(draw);
 
     //Get spectrum data
     spe.getFloatFrequencyData(dataArray);
-    console.log(1.3458 * dataArray.indexOf(Math.max.apply(null, dataArray)));
-
-    //Draw black background
-    canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-
-    //Draw spectrum
-    const barWidth = (canvas.width / bufferLength) * 2.5;
-    let posX = 0;
-    for (let i = 0; i < bufferLength; i++) {
-        const barHeight = (dataArray[i] + 140) * 2;
-        canvasCtx.fillStyle = 'rgb(' + Math.floor(barHeight + 100) + ', 50, 50)';
-        canvasCtx.fillRect(posX, canvas.height - barHeight / 2, barWidth, barHeight / 2);
-        posX += barWidth + 1;
+    freq = (1.3458 * dataArray.indexOf(Math.max.apply(null, dataArray))).toFixed(2);
+    mon.textContent = freq;
+    diff = [];
+    for (let i = 0; i < Object.values(notes).length; i++){
+        diff[i] = Math.abs(Object.values(notes)[i] - freq);
     }
-};
+    n = Object.keys(notes)[diff.indexOf(Math.min(...diff))];
+    note.textContent = n;
+
+}
